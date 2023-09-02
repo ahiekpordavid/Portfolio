e5827bec158c2500b6b0ab3040c7ef3f0fe59184
davidahiekpor <david.ahiekpor@dreamoval.com>
2023-09-02 19:03:31 +0000

    About Me

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import "./About.css";
+import me from "../../media/me1.png";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about">
+      <div className="aboutImage">
+        <img src={me} alt="Me" className="meImage"/>
+      </div>
+      <div className="aboutText">
+        <h1>About Me</h1>
+        <p>
+          I'm a dedicated frontend engineer passionate about crafting remarkable
+          web experiences. With a background in computer science and a love for
+          design. I specialize in turning ideas into interactive,
+          user-friendly web applications. What sets me apart
+          is my versatile skill set, which extends to backend development. I'm
+          well-versed in Express.js and MongoDB, allowing me to create robust
+          RESTful APIs that power seamless interactions between the frontend and
+          backend.
+        </p>
+        <div className="aboutTabs">
+            <div className="aboutTab">
+                <h3>Experience</h3>
+                <div>
+                    <h6>YYYY</h6>
+                    <p>Work</p>
+                </div>
+                <div>
+                    <h6>YYYY</h6>
+                    <p>Work</p>
+                </div>
+            </div>
+            <div className="aboutTab">
+                <h3>Education</h3>
+                <div>
+                    <h6>YYYY</h6>
+                    <p>School</p>
+                </div>
+                <div>
+                    <h6>YYYY</h6>
+                    <p>School</p>
+                </div>
+            </div>
+        </div>
+        <a href="#">Download CV</a>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -4,14 +4,15 @@ import api from "../../media/api.png";
 import code from "../../media/code.png";
 import responsive from "../../media/responsive.png";
 import translate from "../../media/translate.png";
+import ux from "../../media/ux.png";
 
 const Services = () => {
   const data = [
     {
       icon: <img src={code} alt="code"  className="skillIcons"/>,
-      name: "ReactJs Development",
+      name: "Frontend Development",
       description:
-        "Using React JavaScript frameworks for building complex user interfaces for both websites and web apps with app-like experiences.",
+        "Using HTML, CSS, JavaScript or React JavaScript frameworks for building complex user interfaces for both websites and web apps with app-like experiences.",
     },
     {
       icon:<img src={api} alt="Api" className="skillIcons" /> ,
@@ -30,6 +31,18 @@ const Services = () => {
       name: "Internationalization (i18n) and Localization (l10n)",
       description:
      " Implementing support for multiple languages and regions in web applications",
+    },
+    {
+      icon: <img src={code} alt="translate"  className="skillIcons"/>,
+      name: "Backend Development",
+      description:
+     "Building RESTful APIs with Express.js and  MongoDB for storing and retrieving data.",
+    },
+    {
+      icon: <img src={ux} alt="translate"  className="skillIcons"/>,
+      name: " UI/UX Principles",
+      description:
+     "",
     },
   ];
   return (
