266dbcea478a8c2032c74ea8e9890ec2ca996401
davidahiekpor <david.ahiekpor@dreamoval.com>
2023-09-23 12:28:50 +0000

    Responsiveness

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./About.css";
 import me from "../../media/me1.png";
-import CV from "../../media/David Kwashie Ahiekpor.pdf"
+import CV from "../../media/David Kwashie Ahiekpor.pdf";
 import { FormattedMessage } from "react-intl";
-import { useTheme } from '../../utils/theme/ThemeContext';
+import { useTheme } from "../../utils/theme/ThemeContext";
 
 const About = () => {
   const { theme } = useTheme();
@@ -14,37 +14,59 @@ const About = () => {
     backdropFilter: theme.backdropFilter,
   };
   return (
-    <div className="about" style={buttonStyles} >
+    <div className="about" style={buttonStyles}>
       <div className="aboutImage">
-        <img src={me} alt="Me" className="meImage"/>
+        <img src={me} alt="Me" className="meImage" />
       </div>
       <div className="aboutText">
-        <h1><FormattedMessage id="about.title"/></h1>
+        <h1>
+          <FormattedMessage id="about.title" />
+        </h1>
         <p>
-        <FormattedMessage id="about.aboutText"/>
+          <FormattedMessage id="about.aboutText" />
         </p>
         <div className="aboutTabs">
-            <div className="aboutTab">
-                <h3><FormattedMessage id="about.experience.title"/></h3>
-                <div>
-                    <h6><FormattedMessage id="about.experience.YYYY1"/></h6>
-                    <p><FormattedMessage id="about.experience.work1"/></p>
-                </div>
-                <div>
-                    <h6><FormattedMessage id="about.experience.YYYY2"/></h6>
-                    <p><FormattedMessage id="about.experience.work2"/></p>
-                </div>
+          <div className="aboutTab">
+            <h3>
+              <FormattedMessage id="about.experience.title" />
+            </h3>
+            <div>
+              <h6>
+                <FormattedMessage id="about.experience.YYYY1" />
+              </h6>
+              <p>
+                <FormattedMessage id="about.experience.work1" />
+              </p>
             </div>
-            <div className="aboutTab">
-                <h3><FormattedMessage id="about.education.title"/></h3>
-                <div>
-                    <h6><FormattedMessage id="about.education.YYYY1"/></h6>
-                    <p><FormattedMessage id="about.education.school1"/></p>
-                    <h4><FormattedMessage id="about.education.description1"/></h4>
-                </div>
+            <div>
+              <h6>
+                <FormattedMessage id="about.experience.YYYY2" />
+              </h6>
+              <p>
+                <FormattedMessage id="about.experience.work2" />
+              </p>
             </div>
+          </div>
+          <div className="aboutTab">
+            <h3>
+              <FormattedMessage id="about.education.title" />
+            </h3>
+            <div>
+              <h6>
+                <FormattedMessage id="about.education.YYYY1" />
+              </h6>
+              <p>
+                <FormattedMessage id="about.education.school1" />
+              </p>
+              <h4>
+                <FormattedMessage id="about.education.description1" />
+              </h4>
+            </div>
+          </div>
         </div>
-        <a href={CV} download><FormattedMessage id="about.cv"/></a>
+        <a href={CV} download>
+          <FormattedMessage id="about.cv" />
+        </a>
       </div>
     </div>
   );
